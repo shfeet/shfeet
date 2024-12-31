37c9f602462e7775716b87ba7421ce62b8702cc3
epoc <epochousestudios@gmail.com>
2024-12-31 10:51:23 +0100

    ctrl

--- src/Pages/Review.jsx
+++ src/Pages/Review.jsx
@@ -19,7 +19,7 @@ function Review() {
     
     const fetchReviews = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/reviews');
+        const response = await fetch('https://shfeet-backend-3.vercel.app/api/reviews');
         if (!response.ok) {
           throw new Error('Failed to fetch reviews');
         }
